b9962994ddba5abb5d6f88e913d06bd31f5ec8b7
cly <2313681959@qq.com>
2021-03-24 16:50:14 +0800

    spring boot版本修改

--- seata-account-service/seata-account-provider/src/main/java/com/cly/seata/AccountProviderApplication.java
+++ seata-account-service/seata-account-provider/src/main/java/com/cly/seata/AccountProviderApplication.java
@@ -8,7 +8,6 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDubbo
-@EnableDiscoveryClient
 public class AccountProviderApplication {
 
 	public static void main(String[] args) {
