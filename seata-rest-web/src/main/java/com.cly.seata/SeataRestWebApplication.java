942163d0291fd8ff3c1f9fc1209bd3b527948576
cly <2313681959@qq.com>
2021-03-25 11:26:45 +0800

    spring boot版本修改

--- seata-rest-web/src/main/java/com.cly.seata/SeataRestWebApplication.java
+++ seata-rest-web/src/main/java/com.cly.seata/SeataRestWebApplication.java
@@ -3,9 +3,11 @@ package com.cly.seata;
 import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class, DataSourceTransactionManagerAutoConfiguration.class })
 @EnableDubbo
 @EnableDiscoveryClient
 public class SeataRestWebApplication {
